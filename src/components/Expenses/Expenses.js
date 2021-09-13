602c7434bc729415ec69ef7ae324b5482f0bcf50
henriquebcustodio <henriquebcustodio@outlook.com>
2021-09-12 22:17:37 -0300

    Added NewExpense form

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,20 +2,21 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
-function Expenses(props) {
-  return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => {
-        return (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-    </Card>
-  );
-}
+const Expenses = (props) => {
+    return (
+        <Card className="expenses">
+            {props.expenses.map((expense, index) => {
+                return (
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                        key={index}
+                    />
+                );
+            })}
+        </Card>
+    );
+};
 
 export default Expenses;
