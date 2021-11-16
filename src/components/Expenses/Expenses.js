e98fcf7b948cbb156402c6912fccb8a61e58149f
henriquebcustodio <henriquebcustodio@outlook.com>
2021-11-16 20:06:44 -0300

    Added the expenses chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import { useState } from 'react';
 
 const Expenses = (props) => {
@@ -19,6 +20,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selectedYear={filteredYear} onFilterChange={filterChangeHandler} />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList expenses={filteredExpenses} />
         </Card>
     );
